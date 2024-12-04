56f6198e8012211126bb6f7e52c8bc8fff135c5c
IskenderDev <iskenderkadyrjanov@gmail.com>
2024-12-04 14:53:12 +0600

    v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,19 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#B35D6C', 
+          light: '#F3CADB',  
+          dark: '#8C3A46',    
+        },
+        background: '#FFF8F3', 
+      },
+      textColor: {
+        primary: '#B35D6C', 
+      },
+    },
   },
   plugins: [],
 };
-
